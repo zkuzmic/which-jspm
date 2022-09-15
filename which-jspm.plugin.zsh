8c7b693c412b7e29302efbbe9bd21ed470c85ea2
Zach Kuzmic <zkuzmic@indeed.com>
2022-09-15 13:03:43 -0500

    fix bug that was adding the same package manager label more than once

--- which-jspm.plugin.zsh
+++ which-jspm.plugin.zsh
@@ -1,30 +1,37 @@
 WHICH_JPSM_PROMPT=''
+NPM_PROMPT="%K{#cd3534}%F{#ffffff} npm %F{reset}%K{reset} "
+YARN_PROMPT="%K{#2d8ebb}%F{#ffffff} yarn %F{reset}%K{reset} "
+PNPM_PROMPT="%K{#f9ad01}%F{#ffffff} pnpm %F{reset}%K{reset} "
 
 function found_package() {
-  if test -f "yarn.lock"; then
-    WHICH_JPSM_PROMPT+="%K{#2d8ebb}%F{#ffffff} yarn %F{reset}%K{reset} "
+  if test -f "package-lock.json" && [[ "$WHICH_JPSM_PROMPT" != *"$NPM_PROMPT"* ]]; then
+    WHICH_JPSM_PROMPT+=$NPM_PROMPT
   fi
 
-  if test -f "package-lock.json"; then
-    WHICH_JPSM_PROMPT+="%K{#cd3534}%F{#ffffff} npm %F{reset}%K{reset} "
+  if test -f "yarn.lock" && [[ "$WHICH_JPSM_PROMPT" != *"$YARN_PROMPT"* ]]; then
+    WHICH_JPSM_PROMPT+=$YARN_PROMPT
   fi
 
-  if test -f "pnpm-lock.yaml"; then
-    WHICH_JPSM_PROMPT+="%K{#f9ad01}%F{#ffffff} pnpm %F{reset}%K{reset} "
+  if test -f "pnpm-lock.yaml" && [[ "$WHICH_JPSM_PROMPT" != *"$PNPM_PROMPT"* ]]; then
+    WHICH_JPSM_PROMPT+=$PNPM_PROMPT
   fi
 }
-
 
 add-zsh-hook chpwd chpwd_which_jspm
 chpwd_which_jspm() {
   if test -f "package.json";
   then
+    PROMPT_ORIG=${PROMPT%$WHICH_JPSM_PROMPT}
+    JPSM_PROMPT_BEFORE=${PROMPT//$PROMPT_ORIG/}
+
     found_package
-    PROMPT+=$WHICH_JPSM_PROMPT
+    
+    if [[ "$JPSM_PROMPT_BEFORE" != "$WHICH_JPSM_PROMPT" ]]; then
+      PROMPT+=$WHICH_JPSM_PROMPT
+    fi
   elif [ "$WHICH_JPSM_PROMPT" != '' ]
   then
     PROMPT=${PROMPT%$WHICH_JPSM_PROMPT}
     WHICH_JPSM_PROMPT=''
   fi
 }
-
